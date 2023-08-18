3cbc1dd438cab4eacaf42be3cf99985c81db12b7
OverseerAtom <111481829+OverseerAtom@users.noreply.github.com>
2023-08-18 14:30:19 -0700

    redo merge

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,7 +3,6 @@ import { Header } from '../components/js/header';
 import {Footer} from '../components/js/footer'
 import { Reasons } from '../components/js/reasons';
 import { IntroText } from '../components/js/intro-text';
-import { AboutOurCompany } from '../components/js/about-our-company';
 import { ContactUs } from '../components/js/ways-to-contact';
 import '../global.module.css';
 
@@ -13,7 +12,6 @@ const Homepage = () => (
                 <Header/>
                 <IntroText/>
                 <Reasons/>
-                <AboutOurCompany/>
                 <ContactUs/>
                 <Footer/>
         </React.Fragment>
