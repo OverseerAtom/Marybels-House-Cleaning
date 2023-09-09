2abbdae796499ed0ea8eeaa4b36c5c7424d4685f
OverseerAtom <111481829+OverseerAtom@users.noreply.github.com>
2023-09-09 14:16:56 -0700

    created url env file

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Homepage} from './pages/home'
 import { ContactPage } from './pages/contact';
 import { PricePage } from './pages/pricing';
-// import { Services } from './pages/services/services';
+import { Services } from './pages/services';
 import { About } from './pages/about';
 import { BookNow } from './pages/book-now';
+import { PricingQuote } from './pages/pricing-quote-form';
 function App() {
   return (
   <div className='App'>
@@ -14,9 +15,10 @@ function App() {
         <Route path='/' element={<Homepage />}/>
         <Route path='/contact' element={<ContactPage />}/>
         <Route path='/pricing' element={<PricePage />}/>
-        {/* <Route path='/services' element={<Services />}/> */}
+        <Route path='/services' element={<Services />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/book-now' element={<BookNow />}/>
+        <Route path='/pricing/quote' element={<PricingQuote/>}/>
       </Routes>
     </Router>
   </div>
