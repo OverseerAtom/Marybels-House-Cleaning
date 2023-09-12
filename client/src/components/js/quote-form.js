4457a03495fa8679764860b7f3bf75e081ce9156
OverseerAtom <111481829+OverseerAtom@users.noreply.github.com>
2023-09-12 00:01:35 -0700

    form is styled with button

--- client/src/components/js/quote-form.js
+++ client/src/components/js/quote-form.js
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import styles from '../css/quote_form.module.css'
 import axios from 'axios';
-import { Form, FormGroup, FormControl, Row, Col } from "react-bootstrap";
+import { Form, FormGroup, FormControl, Row, Col, Container } from "react-bootstrap";
 import { ToastContainer, toast } from 'react-toastify';
+import design from '../css/button.module.css'
 
-const quoteForm = () =>{
 
-    const [messageSent, setMessageSent] = useState(false)
+const QuoteForm = () => {
+
+    const [messageSent, setMessageSent] = useState(false);
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     const [formData, setFormData] = useState ({
 
@@ -20,6 +23,7 @@ const quoteForm = () =>{
         streetAddress2: "",
         city: "",
         state: "",
+        zipCode: "",
     });
 
     const [formErrors, setFormErrors] = useState ({
@@ -34,6 +38,7 @@ const quoteForm = () =>{
         streetAddress2: "",
         city: "",
         state: "",
+        zipCode: "",
     });
 
     const camelCaseToNormalCase = (camelCaseString) => {
@@ -42,6 +47,20 @@ const quoteForm = () =>{
           return normalCase.charAt(0).toUpperCase() + normalCase.slice(1);
       };
 
+      const handleInputChange = (event) => {
+
+        const {name, value} = event.target;
+    
+        setFormData({
+            ...formData,
+            [name] : value,
+        });
+      };
+    
+      const validateForm = () => {
+        let isValid = true;
+        let errors = {};
+    
       function validateEmail(email) {
         var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         return re.test(String(email).toLowerCase());
@@ -62,17 +81,13 @@ const quoteForm = () =>{
         setFormErrors(errors);
         return isValid;
       };
-
-      const validateForm = () => {
-        let isValid = true;
-        let errors = {};
-      }
+    
 
       const handleSubmit = async (event) => {
         event.preventDefault();
         if (validateForm()) {
             try {
-                const response = await axios.post(REACT_APP_URL_QUOTE, formData);
+                const response = await axios.post("");
 
                 if(response.status === 200 || response.status === 201) {
                     setMessageSent(true);
@@ -87,16 +102,181 @@ const quoteForm = () =>{
             }
             
         }
-      }
+      };
     
-    
-    
-
     return(
-        <div className={styles.mainContainer}>
-
+        <div className={styles.mainContainer} class="container-fluid px-5 py-2 mx-auto" style={{ maxWidth: '1360px', margin: '40px 0px 40px 0px' }}>
+            <ToastContainer position={toast.POSITION.TOP_CENTER}/>
+            <div>
+                <div className={styles.headingBlock}>
+                    <h1>Complete your request.</h1>
+                </div>
+                <div>
+                    <p>Fill out the form below, so we can book a time to visit your resident, and give you a in-person quote for free.</p>
+                </div>
+                <Container>
+                    <Form onSubmit={handleSubmit}>
+                        <Row>
+                            <div className={styles.subHeader}>
+                                <h3>Personal Information:</h3>
+                            </div>
+                            <Col xs={12} md={6}>
+                                <FormGroup controlId='formName' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="name"
+                                    value={FormData.firstName}
+                                    onChange={handleInputChange}
+                                    placeholder="First Name"
+                                    />
+                                    <div className='text-danger'>{formErrors.firstName}</div>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <FormGroup controlId='formName' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="name"
+                                    value={FormData.lastName}
+                                    onChange={handleInputChange}
+                                    placeholder="Last Name"
+                                    />
+                                    <div className='text-danger'>{formErrors.lastName}</div>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12}>
+                                <FormGroup controlId='email' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="email"
+                                    value={FormData.email}
+                                    onChange={handleInputChange}
+                                    placeholder="Email"
+                                    />
+                                    <div className='text-danger'>{formErrors.email}</div>
+                                </FormGroup>
+                            </Col>
+                            <div className={styles.subHeader}>
+                                <h3>Service Information:</h3>
+                            </div>
+                            <Col xs={12} md={4}>
+                                <FormGroup controlId='squareFeet' className={styles.inputBox}>
+                                    <FormControl
+                                    as="select"
+                                    name="squareFeet"
+                                    value={FormData.bedrooms}
+                                    onChange={handleInputChange}
+                                    placeholder='How many Bedrooms?'
+                                    >
+                                    <option value="" disabled selected>Select bedrooms</option>
+                                    <option value="2">Studio</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5+">5+</option>
+                                    </FormControl>
+                                    <div className='text-danger'>{formErrors.squareFeet}</div>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12} md={4}>
+                                <FormGroup controlId='squareFeet' className={styles.inputBox}>
+                                    <FormControl
+                                    as='select'
+                                    name="squareFeet"
+                                    value={FormData.squareFeet}
+                                    onChange={handleInputChange}
+                                    placeholder="How many square feet?"
+                                    >
+                                    <option value="" disabled selected>Select frequency</option>
+                                    <option value="2">Less than 1000sqft</option>
+                                    <option value="1">1000 - 15000sqft</option>
+                                    <option value="2">1500 - 2000sqft</option>
+                                    <option value="3">2000 - 2500 sqft</option>
+                                    <option value="4">2500 - 3000sqft</option>
+                                    <option value="5+">3000sqft +</option>
+                                    <div className='text-danger'>{formErrors.squareFeet}</div>
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12} md={4}>
+                                <FormGroup controlId='frequency' className={styles.inputBox}>
+                                    <FormControl
+                                    as="select"
+                                    name="frequency"
+                                    value={FormData.frequency}
+                                    onChange={handleInputChange}
+                                    placeholder="How frequent?"
+                                    >
+                                    <option value="" disabled selected>Select frequency</option>
+                                    <option value="2">One time service</option>
+                                    <option value="1">1 time a month</option>
+                                    <option value="2">2 times a month</option>
+                                    <option value="3">3 times a month</option>
+                                    <option value="3">4+ times a month</option>
+                                    <option value="4">Airbnb</option>
+                                    <div className='text-danger'>{formErrors.frequency}</div>
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
+                            <div className={styles.subHeader}>
+                                <h3>Address:</h3>
+                            </div>
+                            <Col xs={12}>
+                                <FormGroup controlId='streetAddress' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="streetAddress"
+                                    value={FormData.streetAddress}
+                                    onChange={handleInputChange}
+                                    placeholder="Street Address"
+                                    />
+                                    <div className='text-danger'>{formErrors.streetAddress}</div>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12}>
+                                <FormGroup controlId='streetAddress2' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="streetAddress"
+                                    value={FormData.streetAddress2}
+                                    onChange={handleInputChange}
+                                    placeholder="2nd Street Address"
+                                    />
+                                    <div className='text-danger'>{formErrors.streetAddress2}</div>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <FormGroup controlId='city' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="city"
+                                    value={FormData.city}
+                                    onChange={handleInputChange}
+                                    placeholder="City"
+                                    />
+                                    <div className='text-danger'>{formErrors.city}</div>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <FormGroup controlId='state' className={styles.inputBox}>
+                                    <FormControl
+                                    type='text'
+                                    name="city"
+                                    value={FormData.state}
+                                    onChange={handleInputChange}
+                                    placeholder="State"
+                                    />
+                                    <div className='text-danger'>{formErrors.state}</div>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                    </Form>
+                    <button className={design['button-17']} onClick={() => handleSubmit}>Send Request</button>
+                </Container>
+            </div>
         </div>
     )
 }
 
-export {quoteForm}
+export {QuoteForm}
--- client/src/pages/pricing-quote-form.js
+++ client/src/pages/pricing-quote-form.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from '../components/js/header'
 import { Footer } from '../components/js/footer'
+import { QuoteForm } from '../components/js/quote-form'
 
 const PricingQuote = () => {
     return(
         <React.Fragment>
             <Header/>
+            <QuoteForm/>
             <Footer/>
         </React.Fragment>
     )
