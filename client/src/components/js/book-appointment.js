1350ad3886c291e628c4999150e26b51bf5415cf
OverseerAtom <111481829+OverseerAtom@users.noreply.github.com>
2023-11-07 12:02:51 -0800

    added js and css file

---
+++ client/src/components/js/book-appointment.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../css/book_appointment.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const BookAppointment = () => {
+
+
+    return(
+        <div className={styles.mainContainer}>
+            <div className={styles.box}>
+                <div className={styles.content}>
+                    <
+                </div>
+            </div>
+        </div>
+
+        
+    )
+}
