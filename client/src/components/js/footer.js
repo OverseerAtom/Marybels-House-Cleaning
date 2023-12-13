5231bdd30229a81628b75e77913c6f0ff47b4347
OverseerAtom <111481829+OverseerAtom@users.noreply.github.com>
2023-12-12 22:25:39 -0800

    fixed Footer

--- client/src/components/js/footer.js
+++ client/src/components/js/footer.js
@@ -1,29 +1,42 @@
 import { React, useState } from 'react';
 import styles from "../css/footer.module.css"
+import { Row, Col } from 'react-bootstrap';
 const Footer = () => {
 
     return (
         <div className={styles.mainContainer}>
-            <div className={styles.contentContainer}>
-                <div className={styles.leftContainer}>
-                    <h2>Services</h2>
-                    <p>Basic House Cleaning</p>
-                    <p>Office Cleaning</p>
-                    <p>Deep Cleaning</p>
-                </div>
-                <div className={styles.middleContainer}>
-                    <h2>Information</h2>
-                    <p>Avaliable Cities</p>
-                    <p>Free Quotes</p>
-                    <p>Price Estimation</p>
-                </div>
-                <div className={styles.rightContainer}>
-                    <h2>About Us</h2>
-                    <p>(510) 456 - 5217</p>
-                    <p>support@mhc.com</p>
-                    <p>Mon - Sat: 8:00am - 8:00pm</p>
-                </div>
-            </div>
+            <Row>
+                <Col xs={12} md={6} lg={4}>
+                    <div className={styles.leftContainer}>
+                    <div className={styles.boxHeader}>
+                        <h2>Services</h2>
+                    </div>
+                        <p>Basic House Cleaning</p>
+                        <p>Office Cleaning</p>
+                        <p>Deep Cleaning</p>
+                    </div>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                    <div className={styles.middleContainer}>
+                        <div className={styles.boxHeader}>
+                            <h2>Information</h2>
+                        </div>
+                        <p>Avaliable Cities</p>
+                        <p>Free Quotes</p>
+                        <p>Price Estimation</p>
+                    </div>
+                </Col>
+                <Col xs={12} md={6} lg={4}>
+                    <div className={styles.rightContainer}>
+                        <div className={styles.boxHeader}>
+                            <h2>About Us</h2>
+                        </div>
+                        <p>(510) 456 - 5217</p>
+                        <p>support@mhc.com</p>
+                        <p>Mon - Sat: 8:00am - 8:00pm</p>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
